f6145e03e949adbae8124db80c4d969069083f62
jboz <julienboz@gmail.com>
2019-04-07 16:33:17 +0200

    try

--- ng-app/README.md
+++ ng-app/README.md
@@ -1,6 +1,6 @@
-# NgApp
+# ng-app
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.0.6.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.1.0.
 
 ## Development server
 
--- ng-app/src/app/app.component.ts
+++ ng-app/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { FingerprintAuth } from 'nativescript-fingerprint-auth';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ng-app';
+  private fingerprintAuth: FingerprintAuth;
+
+  public constructor() {
+    this.fingerprintAuth = new FingerprintAuth();
+  }
 }
